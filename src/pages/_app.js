44d22bfb1819c0df922232b05b0039ed910a4ab0
iamclement1 <blessedmarcel1@gmail.com>
2023-04-11 21:17:01 +0100

    'feats: set navbar and footer globally'

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { Footer, Navbar } from '@/components/layouts'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
