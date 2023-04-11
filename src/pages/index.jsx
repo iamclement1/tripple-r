c0cce498ac08bdbd253bb4fbc65b95f115fa1ba8
iamclement1 <blessedmarcel1@gmail.com>
2023-04-11 21:18:06 +0100

    'feats: home page'

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { Navbar } from '@/components/layouts'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  )
+}
