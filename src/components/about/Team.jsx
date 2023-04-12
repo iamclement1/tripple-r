d7d7bce701a41a23b03e6e11e3d4b8c996cebb31
iamclement1 <blessedmarcel1@gmail.com>
2023-04-12 12:21:32 +0100

    'feats: about components and about page'

---
+++ src/components/about/Team.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Team = () => {
+    return (
+        <React.Fragment>
+            <h1 className='text-center text-3xl font-bold text-[#333333] '>
+                Meet our <span className='text-[#E27D60]'>Team</span>
+            </h1>
+            <p className='lg:w-[54rem] mx-14 lg:mx-auto my-3 text-center text-[18x] text-[#767676]'>
+                At Tripple R, we think beauty should be presented in a beautiful way. As a result, we give our clients feelings just as much consideration as the treatments we offer. All of our team members are committed to providing each and every client they serve with the entire Tripple R experience because they recognize the value of Tripple R’s people-focused approach to beauty.
+            </p>
+        </React.Fragment>
+    )
+}
+
+export default Team
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,64 @@
+import Image from 'next/image'
 import React from 'react'
+import img1 from '../assets/images/image55.png'
+import img2 from '../assets/images/image68.png'
+import img3 from '../assets/images/image78.png'
+import { Fade } from 'react-awesome-reveal'
+import { Team } from '@/components/about'
 
 const About = () => {
     return (
-        <div>About</div>
+        <section>
+            <main>
+                <h1 className=' bg-[#F2F2F2] text-center ml-4 lg:ml-36 p-6 
+                text-[#E27D60] font-semibold text-2xl'>
+                    About Us
+                </h1>
+                <p className='lg:w-[49rem] mx-12 lg:mx-auto my-3 text-center text-[18x] text-[#767676] '>
+                    At Tripple R unisex salon, we are committed to going above and beyond at every turn. For us, providing outstanding service is only one part of providing an extraordinary experience. With us having an
+                    amazing experience is guaranteed.
+                </p>
+            </main>
+
+            <main className=' md:flex lg:flex mx-6 md:mx-6 lg:mx-36 md:space-x-8 lg:space-x-12'>
+                <Fade direction='up' triggerOnce cascade>
+                    <div>
+                        <Image src={img1}
+                            alt='productImage'
+                            width={700}
+                            height={700}
+                            className='mb-4 md:mb-0 lg:mb-0'
+                        />
+                    </div>
+                </Fade>
+                <div className='flex flex-col md:space-y-5 lg:space-y-7'>
+                    <Fade direction='up' triggerOnce cascade>
+                        <div>
+                            <Image src={img2}
+                                alt='productImage'
+                                width={500}
+                                height={500}
+                                className='mb-4 md:mb-0 lg:mb-0'
+                            />
+                        </div>
+                    </Fade>
+                    <Fade direction='up' triggerOnce cascade>
+                        <div>
+                            <Image src={img3}
+                                alt='productImage'
+                                width={500}
+                                height={500}
+                                className='mb-4 md:mb-0 lg:mb-0'
+                            />
+                        </div>
+                    </Fade>
+                </div>
+            </main>
+
+            <main className='my-14'>
+                <Team />
+            </main>
+        </section>
     )
 }
 
