0cea90dda9ffe0047d3f4e7fe5fb9a3b08a4c834
iamclement1 <blessedmarcel1@gmail.com>
2023-04-12 21:12:22 +0100

    feats: imported component to the index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,25 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Navbar } from '@/components/layouts'
+import { Client, Navbar, Products, Team } from '@/components/layouts'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <React.Fragment>
+
+      <main className='my-14'>
+        <Team />
+      </main>
+
+      <main className='my-14'>
+        <Client />
+      </main>
+
+      <main className='my-14'>
+        <Products />
+      </main>
+    </React.Fragment>
   )
 }
