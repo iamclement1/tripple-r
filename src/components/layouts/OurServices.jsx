40f02c66c3d89b8a7c6096c9c82d00cf0c038580
iamclement1 <blessedmarcel1@gmail.com>
2023-04-13 20:40:26 +0100

    "update: change key"

--- src/components/layouts/OurServices.jsx
+++ src/components/layouts/OurServices.jsx
@@ -34,12 +34,12 @@ const OurServices = () => {
             <div className=' lg:mx-36 grid grid-cols-1 md:hidden md:grid-cols-3 lg:hidden lg:grid-cols-3
                 gap-2 md:gap-4 lg:gap-4'>
                 {
-                    MobileServices && MobileServices.map((service, index) => {
+                    MobileServices && MobileServices.map((service, item) => {
                         return (
                             <>
                                 <Fade direction='up' triggerOnce cascade>
                                     <div
-                                        key={index}
+                                        key={item}
                                         className=' items-center mb-4 rounded-lg p-4 mt-2 md:mt-4 lg:mt-4'>
                                         <Image src={service.image}
                                             alt='team photograph'
