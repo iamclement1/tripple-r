76a73a10bd4578bb1cdf8b902234cb83d59e0569
iamclement1 <blessedmarcel1@gmail.com>
2023-04-16 15:07:37 +0100

    'update: added price table list table'

--- src/pages/prices.jsx
+++ src/pages/prices.jsx
@@ -1,4 +1,5 @@
 import { Client, Products, Team } from '@/components/layouts'
+import { prices } from '@/utils/Utils'
 import React from 'react'
 
 const Price = () => {
@@ -8,6 +9,25 @@ const Price = () => {
             text-[#E27D60] font-semibold text-2xl'>
                 Prices
             </h1>
+
+
+            <main className='lg:mx-36 md:mx-6 mx-4 text-[#333] font-semibold text-sm md:text-xl lg:text-xl mt-8'>
+                {
+                    prices && prices.map((price, index) => {
+                        return (
+                            <div key={index}
+                                className='flex text-start justify-between border py-6 px-6'>
+                                <p>
+                                    {price.name}
+                                </p>
+                                <p className='text-start'>
+                                    {price.amount}
+                                </p>
+                            </div>
+                        )
+                    })
+                }
+            </main>
 
 
             <main className='my-14'>
